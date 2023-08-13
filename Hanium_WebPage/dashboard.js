fced35ef10fb6fb27fb4321c8d574804ea8e497a
dlghdry2@naver.com <111673196+LeeHongyo@users.noreply.github.com>
2023-08-13 18:14:40 +0900

    Add Dashborad3

--- Hanium_WebPage/dashboard.js
+++ Hanium_WebPage/dashboard.js
@@ -46,4 +46,28 @@
       }
     }
   })
-})()
+})();
+
+let isDateShown = false;  // 상태를 추적하는 전역 변수
+
+function showData() {
+  const dataDisplayElement = document.getElementById('dataDisplay');
+  
+  if (isDateShown) {
+    dataDisplayElement.innerText = "Today";  // 날짜 정보 숨기기
+  } else {
+    const currentDate = new Date();
+    const dateString = currentDate.toLocaleDateString() + " " + currentDate.toLocaleTimeString();
+    dataDisplayElement.innerText = dateString;  // 날짜 정보 표시
+  }
+
+  isDateShown = !isDateShown;  // 상태 전환
+}
+
+
+
+
+
+
+
+
