449098d498f2a6e07e2edf46e9969bbe4b3c9a4f
dlghdry2@naver.com <111673196+LeeHongyo@users.noreply.github.com>
2023-08-13 18:09:41 +0900

    add dashborad

---
+++ dashboard.js
@@ -0,0 +1,49 @@
+/* globals Chart:false */
+
+(() => {
+  'use strict'
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          2435,
+          1234,
+          6335,
+          7777,
+          3489,
+          12378,
+          1924
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          boxPadding: 3
+        }
+      }
+    }
+  })
+})()
